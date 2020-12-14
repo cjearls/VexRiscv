3c1d6226f36ba93d79e19d462e9e0c92b523bffd
Chandler Jearls <cjearls@vt.edu>
2020-12-14 04:26:07 -0500

    The decompressor is probably fixed now, so it should be ready to work properly. I will check soon

--- generateLiteXVexRiscv.sh
+++ generateLiteXVexRiscv.sh
@@ -1,1 +1,1 @@
-sbt "runMain vexriscv.GenCoreDefault --externalInterruptArray=true --csrPluginConfig=linux-minimal" && cat lzCompressNew.v >> VexRiscv.v && cat lzDecompressNew.v >> VexRiscv.v
+sbt "runMain vexriscv.GenCoreDefault --externalInterruptArray=true --csrPluginConfig=linux-minimal" && cat lzCompressNew.v >> VexRiscv.v && cat lzDecompressNew.v >> VexRiscv.v 
