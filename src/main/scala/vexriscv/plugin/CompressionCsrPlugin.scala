baf5e136f3694ff952f348caa4baaaef1fecd915
Chandler Jearls <cjearls@vt.edu>
2020-12-13 19:30:14 -0500

    lz compresor set up, but nothing connected and no software

--- src/main/scala/vexriscv/plugin/CompressionCsrPlugin.scala
+++ src/main/scala/vexriscv/plugin/CompressionCsrPlugin.scala
@@ -6,6 +6,44 @@ import spinal.lib.{Flow, master}
 import vexriscv.plugin.{CsrInterface, Plugin}
 import vexriscv.{DecoderService, Stageable, VexRiscv}
 
+class LzwCompressor() extends BlackBox {
+  val io = new Bundle{
+    val clk = in Bool
+    val reset = in Bool
+    val stop = in Bool
+    val in_valid = in Bool
+    val out_ready = in Bool
+    val in_bits = in UInt(8 bits)
+    val in_ready = out Bool
+    val out_valid = out Bool
+    val out_bits = out UInt(9 bits)
+  }
+
+  // map the current clock domain to the io.clk pin
+  mapClockDomain(clock=io.clk, reset=io.reset)
+  // Set the path to look for the necessary dependency.
+  addRTLPath(s"${sys.env("VEXRISCV_ROOT")}/lzCompressNew.v")
+}
+
+class LzwDecompressor() extends BlackBox {
+  val io = new Bundle{
+    val clk = in Bool
+    val reset = in Bool
+    val in_valid = in Bool
+    val out_ready = in Bool
+    val in_bits = in UInt(9 bits)
+    val in_ready = out Bool
+    val out_valid = out Bool
+    val out_bits = out UInt(16 bits)
+    val dataOutLength = out UInt(2 bits)
+  }
+
+  // map the current clock domain to the io.clk pin
+  mapClockDomain(clock=io.clk, reset=io.reset)
+  // Set the path to look for the necessary dependency.
+  addRTLPath(s"${sys.env("VEXRISCV_ROOT")}/lzDecompressNew.v")
+}
+
 // This code was copied from an example, the actual Csr for interfacing with the 
 // compression and decompression blackbox hasn't been implemented yet.
 class CompressionCsrPlugin extends Plugin[VexRiscv]{
@@ -30,6 +68,9 @@ class CompressionCsrPlugin extends Plugin[VexRiscv]{
       when(writeBack.arbitration.isFiring) {
         instructionCounter := instructionCounter + 1
       }
+
+      val compressor = new LzwCompressor
+      val decompressor = new LzwDecompressor
 
       val csrService = pipeline.service(classOf[CsrInterface])
       csrService.rw(0x8FF, cycleCounter)
