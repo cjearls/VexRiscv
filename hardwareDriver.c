b3d1a29462df77a81c31d741d3dc527e20a9b157
Chandler Jearls <cjearls@vt.edu>
2020-12-15 00:01:16 -0500

    initial, probably flawed, hardware driver code committed to do the most basic possible compression

--- hardwareDriver.c
+++ hardwareDriver.c
@@ -2,55 +2,67 @@
 #include "src/main/c/emulator/src/riscv.h"
 #include <stdbool.h>
 
-struct compressorOutputs {
+#define CHARACTERS 4096
+#define CHARACTER_BITS 8
+
+struct compressorOutputs
+{
 	bool inReady;
 	bool outValid;
-	unsigned int outBits;
+	size_t outBits;
 };
 
-struct decompressorOutputs {
+struct decompressorOutputs
+{
 	bool inReady;
 	bool outValid;
-	unsigned int outBits;
-	unsigned int dataOutLength;
+	size_t outBits;
+	size_t dataOutLength;
 };
 
-void writeCompressorInputs(bool stop, bool inValid, bool outReady, unsigned int inBits){
-	csr_write(0x8FC, stop | (inValid<<1) | (outReady<<2) | (inBits << 3));
+void writeCompressorInputs(bool stop, bool inValid, bool outReady, size_t inBits)
+{
+	csr_write(0x8FC, stop | (inValid << 1) | (outReady << 2) | (inBits << 3));
 }
 
-void writeDecompressorInputs(bool inValid, bool outReady, unsigned int inBits){
+void writeDecompressorInputs(bool inValid, bool outReady, size_t inBits)
+{
 	csr_write(0x8FD, inValid | (outReady << 1) | (inBits << 2));
 }
 
-struct compressorOutputs readCompressorOutputs(){
+struct compressorOutputs readCompressorOutputs()
+{
 	struct compressorOutputs outputs;
-	unsigned int rawData = csr_read(0xCFE);
+	size_t rawData = csr_read(0xCFE);
 	outputs.inReady = 1 & rawData;
 	outputs.outValid = 1 & (rawData >> 1);
 	outputs.outBits = rawData >> 2;
 	return outputs;
 }
 
-struct decompressorOutputs readDecompressorOutputs(){
+struct decompressorOutputs readDecompressorOutputs()
+{
 	struct decompressorOutputs outputs;
-	unsigned int rawData = csr_read(0xCFF);
+	size_t rawData = csr_read(0xCFF);
 	outputs.inReady = 1 & rawData;
 	outputs.outValid = 1 & (rawData >> 1);
-	outputs.outBits = ((1<<16)-1) & (rawData >> 2);
+	outputs.outBits = ((1 << 16) - 1) & (rawData >> 2);
 	outputs.dataOutLength = rawData >> 18;
 	return outputs;
 }
 
-int resetCompressor(){
+int resetCompressor()
+{
 	return csr_read(0xCED);
 }
 
-int resetDecompressor(){
+int resetDecompressor()
+{
 	return csr_read(0xCEE);
 }
 
-int main() {
+int main()
+{
 	// This reads the instruction count register.
 	printf("Hello World!\n");
 	printf("The current cycle count is %d, and the current instruction count is %d\n", csr_read(0x8FF), csr_read(0x8FE));
@@ -59,21 +71,63 @@ int main() {
 	int cycle2 = csr_read(0x8FF);
 	csr_write(0x8FE, 0);
 	int instruction1 = csr_read(0x8FE);
-	int instruction2= csr_read(0x8FE);
-	printf("Cycle1: %d\nCycle2: %d\nInstruction1: %d\nInstruction2: %d\n Cycle difference: %d, Instruction difference: %d\n", cycle1, cycle2, instruction1, instruction2, cycle2-cycle1, instruction2-instruction1);
+	int instruction2 = csr_read(0x8FE);
+	printf("Cycle1: %d\nCycle2: %d\nInstruction1: %d\nInstruction2: %d\n Cycle difference: %d, Instruction difference: %d\n", cycle1, cycle2, instruction1, instruction2, cycle2 - cycle1, instruction2 - instruction1);
 
 	// reading outputs from compressor and decompressor
 	printf("compressor: inready=%d, outvalid=%d, outbits=%d\ndecompressor: inready=%d, outvalid=%d, outbits=%d, dataoutlength=%d\n",
-	readCompressorOutputs().inReady,
-	readCompressorOutputs().outValid,
-	readCompressorOutputs().outBits,
-	readDecompressorOutputs().inReady,
-	readDecompressorOutputs().outValid,
-	readDecompressorOutputs().outBits,
-	readDecompressorOutputs().dataOutLength);
+		   readCompressorOutputs().inReady,
+		   readCompressorOutputs().outValid,
+		   readCompressorOutputs().outBits,
+		   readDecompressorOutputs().inReady,
+		   readDecompressorOutputs().outValid,
+		   readDecompressorOutputs().outBits,
+		   readDecompressorOutputs().dataOutLength);
 
 	printf("compressor reset = %d\n", resetCompressor());
 	printf("decompressor reset = %d\n", resetDecompressor());
+
+	char inCharacterArray[CHARACTERS];
+	char outCharacterArray[CHARACTERS];
+	// This is the easiest compression to perform, all zeroes.
+	for (size_t index = 0; index < CHARACTERS; index++)
+	{
+		inCharacterArray[index] = 0;
+	}
+
+	// This is used to iterate through all the input characters and put them into the compressor.
+	size_t currentInCharacterIndex = 0;
+	// This is used to iterate through all the output characters and put them into the output array.
+	size_t currentOutCharacterIndex = 0;
+	while (currentInCharacterIndex < CHARACTERS)
+	{
+		struct compressorOutputs compOut = readCompressorOutputs();
+		struct decompressorOutputs decompOut = readDecompressorOutputs();
+
+		// Feed in the next character to the compressor input
+		if (compOut.inReady)
+		{
+			printf("Inputting %d to compressor\n", inCharacterArray[currentInCharacterIndex]);
+			writeCompressorInputs(false, true, false, inCharacterArray[currentInCharacterIndex]);
+			currentInCharacterIndex++;
+		}
+		else if (compOut.outValid && decompOut.inReady)
+		{
+			printf("compressor out and decompressor input is %d", compOut.outBits);
+			writeCompressorInputs(false, false, true, 0);
+			writeDecompressorInputs(true, false, compOut.outBits);
+		}
+		else if (decompOut.outValid)
+		{
+			printf("decompressor output is valid, outputting bits %d of dataOutLength %d", decompOut.outBits, decompOut.dataOutLength);
+			writeDecompressorInputs(false, true, 0);
+			for (size_t index = 0; index < decompOut.dataOutLength; index++)
+			{
+				outCharacterArray[currentOutCharacterIndex + index] = decompOut.outBits >> (CHARACTER_BITS * (decompOut.dataOutLength - 1 - index));
+				currentOutCharacterIndex++;
+			}
+		}
+	}
 
 	return 0;
 }
